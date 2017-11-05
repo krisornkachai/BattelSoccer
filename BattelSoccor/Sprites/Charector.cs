d3fbc716d27933f69aaa3c02fa58fb7892cdaf5b
krisornkachai <31123619+krisornkachai@users.noreply.github.com>
2017-11-05 20:56:33 +0700

    bug

--- BattelSoccor/Sprites/Charector.cs
+++ BattelSoccor/Sprites/Charector.cs
@@ -12,9 +12,9 @@ namespace BattelSoccor.Sprites
     class Charecctor : Sprite
     {
 
-        bool up ,jumpup = false;
-        
-        public Bat(Texture2D texture)
+        bool up, jumpup = false;
+
+
 
         public Charecctor(Texture2D texture)
             : base(texture)
@@ -22,7 +22,7 @@ namespace BattelSoccor.Sprites
             Speed = 5f;
         }
 
-        
+
 
 
         public override void Update(GameTime gametime, List<Sprite> sprites)
@@ -36,59 +36,59 @@ namespace BattelSoccor.Sprites
             else if (Keyboard.GetState().IsKeyDown(Input.left)) Velocity.X = -3f;
             else Velocity.X = 0f;
 
-            if (Keyboard.GetState().IsKeyDown(Input.jump)&&up==false)
+            if (Keyboard.GetState().IsKeyDown(Input.jump) && up == false)
             {
                 Velocity.X = -Speed;
             }
-            if(jumpup == true)
+            if (jumpup == true)
             {
                 float i = 1;
                 Position.Y -= 7.5f * i;//up
-                if (Position.Y<140f)
+                if (Position.Y < 140f)
                 {
                     up = true;
                 }
             }
 
-            if(up == true)
+            if (up == true)
             {
                 float i = 1;
                 Velocity.Y += 7.5f * i;  //down
                 jumpup = false;
             }
 
-            if (Position.Y + _texture.Height >=386.5)
+            if (Position.Y + _texture.Height >= 386.5)
             {
                 up = false;
             }
-            if(up == false)
+            if (up == false)
             {
                 Velocity.Y = 0f;
 
 
-            foreach (var sprite in sprites)
-            {
-                if (sprite == this) continue;
+                foreach (var sprite in sprites)
+                {
+                    if (sprite == this) continue;
 
-                if (this.Velocity.X > 0 && this.IsTouchingLeft(sprite))
-                    this.Velocity.X = -this.Velocity.X;
-                if (this.Velocity.X < 0 && this.IsTouchingRight(sprite))
-                    this.Velocity.X = -this.Velocity.X;
-                if (this.Velocity.Y > 0 && this.IsTouchingTop(sprite))
-                    this.Velocity.Y = -this.Velocity.Y;
-                if (this.Velocity.Y < 0 && this.IsTouchingBottom(sprite))
-                    this.Velocity.Y = -this.Velocity.Y;
-            }
-
-
-            if ((Position.X <= 0 || Position.X + _texture.Width >= Game1.screenWidth))
+                    if (this.Velocity.X > 0 && this.IsTouchingLeft(sprite))
+                        this.Velocity.X = -this.Velocity.X;
+                    if (this.Velocity.X < 0 && this.IsTouchingRight(sprite))
+                        this.Velocity.X = -this.Velocity.X;
+                    if (this.Velocity.Y > 0 && this.IsTouchingTop(sprite))
+                        this.Velocity.Y = -this.Velocity.Y;
+                    if (this.Velocity.Y < 0 && this.IsTouchingBottom(sprite))
+                        this.Velocity.Y = -this.Velocity.Y;
+                }
 
 
-            {
-                Velocity.X = -Velocity.X;
-            }
+                if ((Position.X <= 0 || Position.X + _texture.Width >= Game1.screenWidth))
 
-            Position += Velocity;
+
+                {
+                    Velocity.X = -Velocity.X;
+                }
+
+                Position += Velocity;
                 // Position.Y = MathHelper.Clamp(Position.Y, 0, Game1.screenHeingt - _texture.Height);
                 Velocity = Vector2.Zero;
 
@@ -97,3 +97,4 @@ namespace BattelSoccor.Sprites
             }
         }
     }
+}
